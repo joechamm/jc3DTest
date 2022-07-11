0c7e39367fc292b23c20fa54360f2eeb2be00441
Joseph Cunningham <joechamm@outlook.com>
2022-07-11 19:48:28 -0400

    setup cmake a little better

---
+++ src/apps/jc3DCh02_ETC2Comp/src/main.cpp
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <thread>
+#include <vector>
+
+#include "Etc.h"
+#include "EtcImage.h"
+#include "EtcFilter.h"
+#include "EtcFile.h"
+
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#include <helpers/RootDir.h>
+#include <string>
+
+int main()
+{
+	std::string imgFilename = ROOT_DIR + std::string("assets/images/sampleSTB.jpg");
+	int w, h, comp;
+	const uint8_t* img = stbi_load( imgFilename.c_str(), &w, &h, &comp, 4 );
+
+	std::vector<float> rgbaf;
+
+	for ( int i = 0; i != w * h * 4; i+=4 )
+	{
+		rgbaf.push_back( img[i+0] / 255.0f );
+		rgbaf.push_back( img[i+1] / 255.0f );
+		rgbaf.push_back( img[i+2] / 255.0f );
+		rgbaf.push_back( img[i+3] / 255.0f );
+	}
+
+	const auto etcFormat = Etc::Image::Format::RGB8;
+	const auto errorMetric = Etc::ErrorMetric::BT709;
+
+	Etc::Image image( rgbaf.data(), w, h, errorMetric );
+
+	image.Encode( etcFormat, errorMetric, ETCCOMP_DEFAULT_EFFORT_LEVEL, std::thread::hardware_concurrency(), 1024 );
+
+	Etc::File etcFile(
+		"image.ktx",
+		Etc::File::Format::KTX,
+		etcFormat,
+		image.GetEncodingBits(),
+		image.GetEncodingBitsBytes(),
+		image.GetSourceWidth(),
+		image.GetSourceHeight(),
+		image.GetExtendedWidth(),
+		image.GetExtendedHeight()
+	);
+	etcFile.Write();
+
+	return 0;
+}
