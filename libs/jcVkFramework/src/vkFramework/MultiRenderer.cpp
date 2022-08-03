5f3b47d65c3875ba2e99c401df74ec8097f3acdf
Joseph Cunningham <joechamm@outlook.com>
2022-08-03 15:28:26 -0400

    MultiRenderer

--- libs/jcVkFramework/src/vkFramework/MultiRenderer.cpp
+++ libs/jcVkFramework/src/vkFramework/MultiRenderer.cpp
@@ -16,42 +16,280 @@ VKSceneData::VKSceneData ( VulkanRenderContext& ctx, const std::string& meshFile
 	std::vector<VulkanTexture> textures;
 	for ( const auto& f : textureFiles_ )
 	{
-		auto t = asyncLoad ? ctx.resources_.add
+		auto t = asyncLoad ? ctx.resources_.addSolidRGBATexture () : ctx.resources_.loadTexture2D ( f.c_str () );
+#if 0
+		if ( t.image.image != nullptr )
+		{
+			setVkImageName(ctx.vkDev_, t.image.image, f.c_str());
+		}
+#endif
+		
 	}
-	
+
+	if ( asyncLoad )
+	{
+		loadedFiles_.reserve ( textureFiles_.size () );
+
+		taskflow_.for_each_index ( 0u, (uint32_t)textureFiles_.size (), 1u, [this]( int idx )
+			{
+				int w, h;
+				const uint8_t* img = stbi_load ( this->textureFiles_[idx].c_str (), &w, &h, nullptr, STBI_rgb_alpha );
+				if ( !img )
+				{
+					img = genDefaultCheckerboardImage ( &w, &h );
+				}
+				std::lock_guard lock ( loadedFilesMutex_ );
+				loadedFiles_.emplace_back ( LoadedImageData{ idx, w, h, img } );
+			}
+		);
+
+		executor_.run ( taskflow_ );
+	}
+
+	allMaterialTextures_ = fsTextureArrayAttachment ( textures );
+
+	const uint32_t materialSize = static_cast<uint32_t>(sizeof ( MaterialDescription ) * materials_.size ());
+	material_ = ctx.resources_.addStorageBuffer ( materialSize );
+	uploadBufferData ( ctx.vkDev_, material_.memory, 0, materials_.data (), materialSize );
+
+	loadMeshes ( meshFile );
+	loadScene ( sceneFile );	
 }
 
 void VKSceneData::loadScene ( const std::string& sceneFile )
-{}
+{
+	::loadScene ( sceneFile.c_str (), scene_ );
+
+	// prepare draw data buffer
+	for ( const auto& c : scene_.meshes_ )
+	{
+		auto material = scene_.materialForNode_.find ( c.first );
+		if ( material == scene_.materialForNode_.end () ) continue;
+		
+		shapes_.push_back (
+			DrawData{
+				.meshIndex = c.second,
+				.materialIndex = material->second,
+				.LOD = 0,
+				.indexOffset = meshData_.meshes_[c.second].indexOffset,
+				.vertexOffset = meshData_.meshes_[c.second].vertexOffset,
+				.transformIndex = c.first
+			}
+		);
+	}
+
+	shapeTransforms_.resize ( shapes_.size () );
+	transforms_ = ctx_.resources_.addStorageBuffer ( shapes_.size () * sizeof ( glm::mat4 ) );
+
+	recalculateAllTransforms ();
+	uploadGlobalTransforms ();
+}
 
 void VKSceneData::loadMeshes ( const std::string & meshFile )
-{}
+{
+	MeshFileHeader header = loadMeshData ( meshFile.c_str (), meshData_ );
+
+	const uint32_t indexBufferSize = header.indexDataSize;
+	uint32_t vertexBufferSize = header.vertexDataSize;
+
+	const uint32_t offsetAlignment = getVulkanBufferAlignment ( ctx_.vkDev_ );
+	if ( (vertexBufferSize & (offsetAlignment - 1)) != 0 )
+	{
+		const size_t numFloats = (offsetAlignment - (vertexBufferSize & (offsetAlignment - 1))) / sizeof ( float );
+		for ( size_t i = 0; i != numFloats; i++ )
+		{
+			meshData_.vertexData_.push_back ( 0 );
+		}
+		vertexBufferSize = (vertexBufferSize + offsetAlignment) & ~(offsetAlignment - 1);
+	}
+	
+	VulkanBuffer storage = ctx_.resources_.addStorageBuffer ( vertexBufferSize + indexBufferSize );
+	uploadBufferData ( ctx_.vkDev_, storage.memory, 0, meshData_.vertexData_.data (), vertexBufferSize );
+	uploadBufferData ( ctx_.vkDev_, storage.memory, vertexBufferSize, meshData_.indexData_.data (), indexBufferSize );
+
+	vertexBuffer_ = BufferAttachment{
+		.dInfo_ = {
+			.type_ = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER,
+			.shaderStageFlags_ = VK_SHADER_STAGE_VERTEX_BIT
+		},
+		.buffer_ = storage,
+		.offset_ = 0,
+		.size_ = vertexBufferSize
+	};
+
+	indexBuffer_ = BufferAttachment{
+		.dInfo_ = {
+			.type_ = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER,
+			.shaderStageFlags_ = VK_SHADER_STAGE_VERTEX_BIT
+		},
+		.buffer_ = storage,
+		.offset_ = vertexBufferSize,
+		.size_ = indexBufferSize
+	};
+}
 
 void VKSceneData::convertGlobalToShapeTransforms ()
-{}
+{
+	// fill the shapeTransforms_ array from globalTransforms_
+	size_t i = 0; 
+	for ( const auto& c : shapes_ )
+	{
+		shapeTransforms_[i++] = scene_.globalTransforms_[c.transformIndex];
+	}
+}
 
 void VKSceneData::recalculateAllTransforms ()
-{}
+{
+	// force recalculation of global transformations
+	markAsChanged ( scene_, 0 );
+	recalculateGlobalTransforms ( scene_ );
+
+}
 
 void VKSceneData::uploadGlobalTransforms ()
-{}
+{
+	convertGlobalToShapeTransforms ();
+	uploadBufferData ( ctx_.vkDev_, transforms_.memory, 0, shapeTransforms_.data (), transforms_.size );
+}
 
 void VKSceneData::updateMaterial ( int matIdx )
-{}
+{
+	uploadBufferData ( ctx_.vkDev_, material_.memory, matIdx * sizeof ( MaterialDescription ), materials_.data () + matIdx, sizeof ( MaterialDescription ) );
+}
 
-MultiRenderer::MultiRenderer ( VulkanRenderContext& ctx, VKSceneData& sceneData, const std::string& vtxShaderFile, const std::string& fragShaderFile, const std::vector<VulkanTexture>& outputs, RenderPass screenRenderPass, const std::vector<BufferAttachment>& auxBuffers, const std::vector<TextureAttachment>& auxTextures )
-{}
+MultiRenderer::MultiRenderer ( 
+	VulkanRenderContext& ctx,
+	VKSceneData& sceneData, 
+	const std::string& vtxShaderFile, const std::string& fragShaderFile, 
+	const std::vector<VulkanTexture>& outputs, 
+	RenderPass screenRenderPass, 
+	const std::vector<BufferAttachment>& auxBuffers, const std::vector<TextureAttachment>& auxTextures ) 
+	: Renderer(ctx)
+	, sceneData_(sceneData)
+{
+	const PipelineInfo pInfo = initRenderPass ( PipelineInfo{}, outputs, screenRenderPass, ctx.screenRenderPass_ );
+
+	const uint32_t indirectDataSize = static_cast<uint32_t>(sceneData_.shapes_.size () * sizeof ( VkDrawIndirectCommand ));
+
+	const size_t imgCount = ctx.vkDev_.swapchainImages.size ();
+	uniforms_.resize ( imgCount );
+	shape_.resize ( imgCount );
+	indirect_.resize ( imgCount );
+
+	descriptorSets_.resize ( imgCount );
+
+	const uint32_t shapeSize = static_cast<uint32_t>(sceneData_.shapes_.size () * sizeof ( DrawData ));
+	const uint32_t uniformBufferSize = sizeof ( ubo_ );
+
+	std::vector<TextureAttachment> textureAttachments;
+	if ( sceneData_.envMap_.width ) textureAttachments.push_back ( fsTextureAttachment ( sceneData_.envMap_ ) );
+	if ( sceneData_.envMapIrradiance_.width ) textureAttachments.push_back ( fsTextureAttachment ( sceneData_.envMapIrradiance_ ) );
+	if ( sceneData_.brdfLUT_.width ) textureAttachments.push_back ( fsTextureAttachment ( sceneData_.brdfLUT_ ) );
+
+	for ( const auto& t : auxTextures )
+	{
+		textureAttachments.push_back ( t );
+	}
+
+	DescriptorSetInfo dsInfo = {
+		.buffers_ = {
+			uniformBufferAttachment ( VulkanBuffer {}, 0, uniformBufferSize, VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT ),
+			sceneData_.vertexBuffer_,
+			sceneData_.indexBuffer_,
+			storageBufferAttachment ( VulkanBuffer {}, 0, shapeSize, VK_SHADER_STAGE_VERTEX_BIT ),
+			storageBufferAttachment ( sceneData_.material_, 0, static_cast<uint32_t>(sceneData_.material_.size), VK_SHADER_STAGE_FRAGMENT_BIT ),
+			storageBufferAttachment ( sceneData_.transforms_, 0, static_cast<uint32_t>(sceneData_.transforms_.size), VK_SHADER_STAGE_VERTEX_BIT )
+		},
+		.textures_ = textureAttachments,
+		.textureArrays_ = { sceneData_.allMaterialTextures_ }
+	};
+			
+	for ( const auto& b : auxBuffers )
+	{
+		dsInfo.buffers_.push_back ( b );
+	}
+
+	descriptorSetLayout_ = ctx.resources_.addDescriptorSetLayout ( dsInfo );
+	descriptorPool_ = ctx.resources_.addDescriptorPool ( dsInfo, (uint32_t)imgCount );
+
+	for ( size_t i = 0; i != imgCount; i++ )
+	{
+		uniforms_[i] = ctx.resources_.addUniformBuffer ( uniformBufferSize );
+		indirect_[i] = ctx.resources_.addIndirectBuffer ( indirectDataSize );
+		updateIndirectBuffers (i);
+
+		shape_[i] = ctx.resources_.addStorageBuffer ( shapeSize );
+		uploadBufferData ( ctx.vkDev_, shape_[i].memory, 0, sceneData_.shapes_.data (), shapeSize );
+
+		dsInfo.buffers_[0].buffer_ = uniforms_[i];
+		dsInfo.buffers_[3].buffer_ = shape_[i];
+
+		descriptorSets_[i] = ctx.resources_.addDescriptorSet ( descriptorPool_, descriptorSetLayout_ );
+		ctx.resources_.updateDescriptorSet ( descriptorSets_[i], dsInfo );
+	}
+
+	initPipeline ( getShaderFilenamesWithRoot ( "assets/shaders/VK07.vert", "assets/shaders/VK07.frag" ), pInfo );
+}
 
 void MultiRenderer::fillCommandBuffer ( VkCommandBuffer cmdBuffer, size_t currentImage, VkFramebuffer fb, VkRenderPass rp )
-{}
+{
+	beginRenderPass ( (rp != VK_NULL_HANDLE) ? rp : renderPass_.handle_, (fb != VK_NULL_HANDLE) ? fb : framebuffer_, cmdBuffer, currentImage );
+
+	/* For CountKHR (Vulkan 1.1) we may use indirect rendering with GPU-based object counter */
+	/// vkCmdDrawIndirectCountKHR(commandBuffer, indirectBuffers_[currentImage], 0, countBuffers_[currentImage], 0, shapes.size(), sizeof(VkDrawIndirectCommand));
+	/* For Vulkan 1.0 vkCmdDrawIndirect is enough */
+	vkCmdDrawIndirect ( cmdBuffer, indirect_[currentImage].buffer, 0, static_cast<uint32_t>(sceneData_.shapes_.size ()), sizeof ( VkDrawIndirectCommand ) );
+
+	vkCmdEndRenderPass ( cmdBuffer );
+}
 
 void MultiRenderer::updateBuffers ( size_t currentImage )
-{}
+{
+	updateUniformBuffer ( (uint32_t)currentImage, 0, sizeof ( ubo_ ), &ubo_ );
+}
 
 void MultiRenderer::updateIndirectBuffers ( size_t currentImage, bool* visibility )
-{}
+{
+	VkDrawIndirectCommand* data = nullptr;
+	vkMapMemory ( ctx_.vkDev_.device, indirect_[currentImage].memory, 0, sizeof ( VkDrawIndirectCommand ), 0, (void**)&data );
+
+	const uint32_t size = static_cast<uint32_t>(sceneData_.shapes_.size ());
+
+	for ( uint32_t i = 0; i != size; i++ )
+	{
+		const uint32_t j = sceneData_.shapes_[i].meshIndex;
+
+		const uint32_t lod = sceneData_.shapes_[i].LOD;
+		data[i] = {
+			.vertexCount = sceneData_.meshData_.meshes_[j].getLODIndicesCount ( lod ),
+			.instanceCount = visibility ? (visibility[i] ? 1u : 0u) : 1u,
+			.firstVertex = 0,
+			.firstInstance = i
+		};
+	}
+	
+	vkUnmapMemory ( ctx_.vkDev_.device, indirect_[currentImage].memory );
+}
 
 bool MultiRenderer::checkLoadedTextures ()
 {
-	return false;
+	VKSceneData::LoadedImageData data;
+
+	{
+		std::lock_guard lock ( sceneData_.loadedFilesMutex_ );
+
+		if ( sceneData_.loadedFiles_.empty () )
+		{
+			return false;
+		}
+		
+		data = sceneData_.loadedFiles_.back ();
+
+		sceneData_.loadedFiles_.pop_back ();
+	}
+
+	this->updateTexture ( data.index_, ctx_.resources_.addRGBATexture ( data.w_, data.h_, const_cast<uint8_t*>(data.img_) ) );
+
+	stbi_image_free ( (void*)data.img_ );
+	return true;
 }
